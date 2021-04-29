5000f14db35a3f8c9809b9c923b9831342d21b73
innpei <43521839+innpei@users.noreply.github.com>
2021-04-29 17:58:44 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -129,6 +129,7 @@ class BaseModel(ABC):
         """Return visualization images. train.py will display these images with visdom, and save the images to a HTML"""
         visual_ret = OrderedDict()
         for name in self.visual_names:
+            print(name) #for debug
             if isinstance(name, str):
                 visual_ret[name] = getattr(self, name)
         return visual_ret
